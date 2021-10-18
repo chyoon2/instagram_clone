af58b60bb2eb4cdba603b438245846ec69804508
Chris Yoon <chy.yoon@gmail.com>
2021-10-17 21:43:10 -0700

    removing semanitc ui segmtent grid from components

--- InstagramClone/client/src/components/Header.js
+++ InstagramClone/client/src/components/Header.js
@@ -6,21 +6,33 @@ import "../css/headerStyles.css";
 
 const Header = () => {
   return (
-    <div className='parent'>
-      <Segment className='scales'>
-        <Grid columns={3}>
-          <Grid.Column color={"red"}>
-            <Image src={InstagramScriptLogo} size={"tiny"} />
-          </Grid.Column>
-          <Grid.Column textAlign='center'>
-            <Input placeholder='Search...' />
-          </Grid.Column>
-          <Grid.Column textAlign='right'>
-            <IconMenu />
-          </Grid.Column>
-        </Grid>
-      </Segment>
+    <div className='grid-contaner'>
+      <div className='grid-item grid-item logo'>
+        <img src={InstagramScriptLogo} alt='Logo' />
+      </div>
+
+      <div className='grid-item grid-item search'>
+        <Input placeholder='Search...' />
+      </div>
+
+      <div className='grid-item grid-item iconMenu'>
+        <IconMenu />
+      </div>
     </div>
   );
 };
 export default Header;
+
+// <Segment className='scales'>
+// <Grid columns={3}>
+//   <Grid.Column color={"red"}>
+//     <Image src={InstagramScriptLogo} size={"tiny"} />
+//   </Grid.Column>
+//   <Grid.Column textAlign='center'>
+//     <Input placeholder='Search...' />
+//   </Grid.Column>
+//   <Grid.Column textAlign='right'>
+//     <IconMenu />
+//   </Grid.Column>
+// </Grid>
+// </Segment>
--- InstagramClone/client/src/components/IconMenu.js
+++ InstagramClone/client/src/components/IconMenu.js
@@ -4,14 +4,17 @@ import PortfolioPic from "../img/PortfolioPic.jpg";
 
 const IconMenu = () => {
   return (
-    <div>
+    <>
       <i className='home icon' />
       <i className='facebook messenger icon' />
       <i className='compass icon' />
       <i className='heart outline icon' />
-      <Image src={PortfolioPic} size={"tiny"} circular />
-    </div>
+    </>
   );
 };
 
 export default IconMenu;
+
+{
+  /* <Image src={PortfolioPic} size={"small"} circular /> */
+}
